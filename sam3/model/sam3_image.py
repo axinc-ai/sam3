639de149265cf15c3947abe3fe0180c536f7fed4
Brian Gontowski <brian.gontowski@axinc.jp>
2025-12-09 16:35:47 +0900

    Progress with onxx

--- sam3/model/sam3_image.py
+++ sam3/model/sam3_image.py
@@ -14,7 +14,11 @@ from sam3.model.vl_combiner import SAM3VLBackbone
 from sam3.perflib.nms import nms_masks
 
 from sam3.train.data.collator import BatchedDatapoint, collate_fn_api
-from sam3.train.data.sam3_image_dataset import Datapoint
+from sam3.train.data.sam3_image_dataset import (
+    Datapoint,
+    FindQueryLoaded,
+    InferenceMetadata,
+)
 
 from .act_ckpt_utils import activation_ckpt_wrapper
 
@@ -531,7 +535,22 @@ class Sam3Image(torch.nn.Module):
     def forward(self, input: BatchedDatapoint):
         if not isinstance(input, BatchedDatapoint):
             input = collate_fn_api([
-                Datapoint(find_queries=[], images=[input]),
+                Datapoint(find_queries=[
+                    FindQueryLoaded(
+                        query_text='',
+                        image_id=0,
+                        object_ids_output=[],
+                        is_exhaustive=False,
+                        inference_metadata=InferenceMetadata(
+                            coco_image_id=0,
+                            original_image_id=0,
+                            original_category_id=0,
+                            original_size=(0, 0),
+                            object_id=0,
+                            frame_index=0,
+                        ),
+                    ),
+                ], images=input),
             ], 'input')['input']
         device = self.device
         backbone_out = {"img_batch_all_stages": input.img_batch}
